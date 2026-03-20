0bcaa3355c95f2a115c8cdf094bd7d97ea52aced
TDC <trevordcooley@gmail.com>
2026-03-19 20:51:55 -0400

    Add closure tests

--- closure/py.py
+++ closure/py.py
@@ -9,3 +9,29 @@ def foo():
 aVar = foo()
 
 print(aVar(7))
+
+
+def counter():
+    count = 0
+
+    def increment():
+        nonlocal count
+        count += 1
+        return count
+
+    return increment
+
+c = counter()
+print(c())  # 1
+print(c())  # 2
+print(c())  # 3
+
+aString = "Hello World"
+
+temp = aString.split()
+
+print(temp)
+
+dog = "".join(temp)
+
+print(dog)
